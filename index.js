b7a0b7023aa76435eb7ba3db8f73114e10a33a94
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-02-07 19:04:06 +0200

    exercises 3.2.-3.3.

--- index.js
+++ index.js
@@ -27,7 +27,26 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  res.send(
+  `The Phonebook has info of ${persons.length} people <br></br>
+   ${new Date().toString()}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+///api/persons/:id, jos ei id niin asianmukainen statuskoodi
+//yksittäisten puhelinnumerotiedon näyttäminen
