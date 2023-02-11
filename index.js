210d04156c0052cc83cb8de316fadb909a8e96d5
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-02-11 18:11:02 +0200

    updating for render

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length]-:response-time ms :content'))
-app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('content', function (request, response) {
   return JSON.stringify(request.body)
@@ -31,7 +36,7 @@ let persons = [
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello hello</h1>')
+  res.send('<h1>Hello world! :)</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
@@ -58,7 +63,7 @@ app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
   
-    response.status(204).end()
+    res.status(204).end()
   })
 
 app.post('/api/persons', (req, res) => {
@@ -87,6 +92,8 @@ app.post('/api/persons', (req, res) => {
   console.log(person)
   res.json(person)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
